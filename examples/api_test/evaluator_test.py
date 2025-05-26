9a0b064547f988f70b34893722843fac64914f2a
wzhu8410 <wzhu8410@gmail.com>
2025-05-26 16:24:02 -0700

    Getting started on Eval class (#88)
    * Getting started with evaluation api class
    
    * Updated evaluator class to include post evaluation
    
    * Improved documentation
    
    ---------
    
    Co-authored-by: Wangkai Zhu <wzhu8410@student.ubc.ca>

--- examples/api_test/evaluator_test.py
+++ examples/api_test/evaluator_test.py
@@ -10,23 +10,39 @@ from src.d2d import D2DEvaluator
 
 def main_eval():
     evaluator = D2DEvaluator(
-        model="gpt-4.1-mini",
+        model="gpt-4o-mini",
         temperature=0.0
     )
 
-    # Define paths (adjust as necessary)
-    log_input_path = os.path.join(root_dir, "results", "D2D_interview_48_generator_log_2025-05-23_14-45.txt")
+    # Define paths
+    log_input_path = os.path.join(root_dir, "results", "D2D_survey_48_generator_log_2025-05-26_14-00.txt")
     context_output_path = os.path.join(root_dir, "eval_results", "retrieved_contexts.csv")
-    rag_csv_path = os.path.join(root_dir, "results", "D2D_interview_48_2025-05-23_14-45.csv")
+    rag_csv_path = os.path.join(root_dir, "results", "D2D_survey_48_responses_2025-05-26_14-00.csv")
     ref_csv_path = os.path.join(root_dir, "data", "private_data", "references", "responses_48.csv")
     eval_output_path = os.path.join(root_dir, "eval_results", "eval_output.csv")
 
-    evaluator.evaluate(
+    # Start processing evaluation
+    results = evaluator.evaluate(
         log_input_path=log_input_path,
         context_output_path=context_output_path,
         rag_csv_path=rag_csv_path,
         ref_csv_path=ref_csv_path,
         eval_output_path=eval_output_path
+    )
+
+    # Post-process results
+    weights = {
+        "correctness": 0.3,
+        "faithfulness": 0.2,
+        "precision": 0.2,
+        "recall": 0.2,
+        "relevance": 0.1
+    }
+    output_prefix = os.path.join(root_dir, "eval_results", "post_eval")
+    evaluator.post_process_results(
+        results=results,
+        weights=weights,
+        output_prefix=output_prefix
     )
 
 if __name__ == "__main__":
