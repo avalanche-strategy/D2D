3115ed96d9f0cf0ea75f76abca40b8f037f52b75
dlam519 <dlam519@student.ubc.ca>
2025-06-25 05:52:34 -0700

    Remove outdated commented code for manual .env loading.
    The removed code block was unnecessary and marked as temporary. Its functionality is already covered by `load_dotenv()`, making the additional code redundant. Cleaning this up improves code readability and maintenance.

--- src/d2d/__init__.py
+++ src/d2d/__init__.py
@@ -24,26 +24,6 @@ Modules:
 
 from dotenv import load_dotenv
 
-"""
-==================================================================================================
-Comment it for now, DO NOT DELETE will come back to it when we need to deliver a .whl file
-==================================================================================================
-import os
-# Manually load .env file from the current working directory
-# dotenv_path = os.path.join(os.getcwd(), '.env')
-# if os.path.exists(dotenv_path):
-#     with open(dotenv_path, 'r') as f:
-#         for line in f:
-#             if line.strip() and not line.startswith('#'):
-#                 try:
-#                     key, value = line.strip().split('=', 1)
-#                     os.environ[key] = value
-#                 except ValueError:
-#                     print(f"Skipping malformed line: {line.strip()}")
-# else:
-#     print(f".env file not found at {dotenv_path}")
-"""
-
 # Make sure it loads the .env file if it's not from the current working directory
 load_dotenv()
 
